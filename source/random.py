02477e437435ab444223a94935e283aa3b7dc60a
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-07-04 10:43:44 -0600

    Add random capital letter, alphanumeric commands

--- source/random.py
+++ source/random.py
@@ -10,6 +10,12 @@ def generate_random_lowercase_letter() -> str:
 def generate_random_character() -> str:
     return chr(random.randint(32, 126))
 
+def generate_random_alpha_numeric_character():
+    lowercase_letters = [chr(i) for i in range(97, 123)]
+    digits = [str(i) for i in range(10)]
+    uppercase_letters = [letter.upper() for letter in lowercase_letters]
+    return random.choice(lowercase_letters + digits + uppercase_letters)
+
 module = Module()
 @module.action_class
 class Actions:
@@ -24,3 +30,12 @@ class Actions:
     def random_insert_random_character():
         """Insert a random character"""
         actions.insert(generate_random_character())
+
+    def random_insert_random_upper_case_letter():
+        """Insert a random uppercase letter"""
+        upper_case_letter = generate_random_lowercase_letter().upper()
+        actions.insert(upper_case_letter)
+
+    def random_insert_random_alphanumeric_character():
+        """Insert a random alphanumeric character"""
+        actions.insert(generate_random_alpha_numeric_character())
--- source/random.talon
+++ source/random.talon
@@ -3,3 +3,5 @@ not tag: user.exam_mode
 random digit: user.random_insert_random_digit()
 random letter: user.random_insert_random_lowercase_letter()
 random (char|character): user.random_insert_random_character()
+random (cap|capital) letter: user.random_insert_random_upper_case_letter()
+random alpha (numb|numeric): user.random_insert_random_alphanumeric_character()
